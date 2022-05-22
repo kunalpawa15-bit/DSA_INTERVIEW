612f8966a005fe8b667ca23645e110ac726d6a5a
kunalpawa15-bit <83181573+kunalpawa15-bit@users.noreply.github.com>
2022-05-22 14:53:30 +0530

    Time: 168 ms (10.98%), Space: 46.7 MB (6.34%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
@@ -3,18 +3,22 @@ public:
     vector<int> findDuplicates(vector<int>& nums) {
         
         vector<int>v;
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-1;i++)
+        map<int,int>m;
+        
+        for(int i=0;i<nums.size();i++)
         {
-            if(nums[i]==nums[i+1])
+            m[nums[i]]++;
+        }
+        
+        for(auto it:m)
+        {
+            if(it.second==2)
             {
-                v.push_back(nums[i]);
+                v.push_back(it.first);
             }
         }
-           return v;
         
-        
-        
+        return v;
         
     }
 };
