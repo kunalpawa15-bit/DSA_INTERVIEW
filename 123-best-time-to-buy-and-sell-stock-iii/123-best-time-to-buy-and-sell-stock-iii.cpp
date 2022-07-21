86117d227ee16660459bb48728cc6a81bdaeaf51
kunalpawa15-bit <83181573+kunalpawa15-bit@users.noreply.github.com>
2022-07-21 11:34:10 +0530

    Time: 1525 ms (5.01%), Space: 207.2 MB (24.95%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -2,27 +2,44 @@ class Solution {
 public:
     int maxProfit(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
-        return f(0,1,nums,2,dp);
-    }
-    
-    int f(int i,int buy,vector<int>&nums,int cap,vector<vector<vector<int>>>&dp)
-    {
-        int n = nums.size();
-        if(cap==0)return 0;
-        if(i==n)return 0;
-        int profit = 0;
-        if(dp[i][buy][cap]!=-1)return dp[i][buy][cap];
-        
-        if(buy)
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+       /* for(int i=0;i<n;i++)
         {
-            profit = max((-nums[i] + f(i+1,0,nums,cap,dp)) , (0+f(i+1,1,nums,cap,dp)));
+            for(int buy=0;buy<=1;buy++)
+            {
+                dp[i][buy][0]= 0;
+            }
         }
-        else
+         for(int buy=0;i<=1;i++)
         {
-            profit = max((nums[i] + f(i+1,1,nums,cap-1,dp)) , (0,f(i+1,0,nums,cap,dp)));
+            for(int cap=0;cap<=2;buy++)
+            {
+                dp[n][buy][cap]= 0;
+            }
+        }*/
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int buy=0;buy<=1;buy++)
+            {
+                for(int cap=1;cap<=2;cap++)
+                {
+                    if(buy)
+                    {
+                          dp[i][buy][cap] = max((-nums[i] + dp[i+1][0][cap]) , (0+dp[i+1][1][cap]));
+                    }
+                    else
+                    {
+                          dp[i][buy][cap] = max((nums[i] + dp[i+1][1][cap-1]) , (0,dp[i+1][0][cap]));
+                    }
+                }
+            }
         }
         
-        return dp[i][buy][cap] = profit;
+         
+        
+        return dp[0][1][2] ;
+        
+        
     }
+
 };
