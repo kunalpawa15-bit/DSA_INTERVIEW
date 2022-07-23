74abec0c01cc7dcda90e0eba5836da3dc74a7d5e
kunalpawa15-bit <83181573+kunalpawa15-bit@users.noreply.github.com>
2022-07-23 11:04:45 +0530

    Time: 39 ms (42.89%), Space: 13 MB (41.14%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -4,19 +4,23 @@ public:
      
         int n = text1.length();
         int m = text2.length();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        for(int j=0;j<=m;j++)dp[0][j]=0;
+        for(int i=0;i<=n;i++)dp[i][0]=0;
         
-        return f(n-1,m-1,text1,text2,dp);
+        for(int ind1=1;ind1<=n;ind1++)
+        {
+            for(int ind2=1;ind2<=m;ind2++)
+            {
+                 if(dp[ind1][ind2]!=-1)return dp[ind1][ind2];
+        
+                 if(text1[ind1-1]==text2[ind2-1])
+                    dp[ind1][ind2] =  1+dp[ind1-1][ind2-1];
+                 else dp[ind1][ind2] = 0 + max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
+            }
+        }
+        
+        return dp[n][m];
     }
     
-    int f(int ind1,int ind2,string &s1,string &s2,vector<vector<int>>&dp)
-    {
-        if(ind1<0 || ind2<0)return 0;
-        if(dp[ind1][ind2]!=-1)return dp[ind1][ind2];
-        
-        if(s1[ind1]==s2[ind2])
-            return dp[ind1][ind2] =  1+f(ind1-1,ind2-1,s1,s2,dp);
-        
-        return dp[ind1][ind2] = 0 + max(f(ind1-1,ind2,s1,s2,dp),f(ind1,ind2-1,s1,s2,dp));
-    }
 };
