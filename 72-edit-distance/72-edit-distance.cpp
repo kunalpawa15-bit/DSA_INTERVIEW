e164beb6005782f7f2dfb8675d4b4c04ebb82b71
kunalpawa15-bit <83181573+kunalpawa15-bit@users.noreply.github.com>
2022-07-23 18:59:46 +0530

    Time: 19 ms (58.64%), Space: 8.9 MB (65.51%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -3,19 +3,24 @@ public:
     int minDistance(string s1, string s2) {
         int n = s1.length();
         int m = s2.length();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return f(n-1,m-1,s1,s2,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        
+        for(int j=0;j<=m;j++)dp[0][j] = j;
+        for(int i=0;i<=n;i++)dp[i][0] = i;
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                 
+               if(s1[i-1]==s2[j-1])
+                    dp[i][j] = 0+dp[i-1][j-1];
+               else  
+                    dp[i][j] =  1 + min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]));
+            }
+        }
+        
+        return dp[n][m];
     }
     
-    int f(int i,int j,string &s1,string &s2,vector<vector<int>>&dp)
-    {
-        if(i<0)return j+1;
-        if(j<0)return i+1;
-        
-        if(dp[i][j]!=-1)return dp[i][j];
-        
-        if(s1[i]==s2[j])
-            return dp[i][j] = 0+f(i-1,j-1,s1,s2,dp);
-        return dp[i][j] =  1 + min(f(i-1,j,s1,s2,dp),min(f(i,j-1,s1,s2,dp),f(i-1,j-1,s1,s2,dp)));
-    }
 };
