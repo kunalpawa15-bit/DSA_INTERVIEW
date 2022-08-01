4cbfdbdd56c070acaf4ce17350b7ed7f2e70eeaf
kunalpawa15-bit <83181573+kunalpawa15-bit@users.noreply.github.com>
2022-08-01 09:25:54 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (26.54%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -4,17 +4,17 @@ public:
         vector<vector<int>>dp(m,vector<int>(n,-1));
         return f(m-1,n-1,dp);
     }
-private:
-    int f(int i,int j,vector<vector<int>>&dp )
-    {
-        if(i==0 && j==0)return 1;
+    
+    
+    int f(int i,int j,vector<vector<int>>&dp)
+    {   if(i==0 && j==0)return 1;
         if(i<0 || j<0)return 0;
+        
         
         if(dp[i][j]!=-1)return dp[i][j];
         
+        int left =  f(i,j-1,dp);
         int up = f(i-1,j,dp);
-        int left = f(i,j-1,dp);
-        
-        return dp[i][j] = up+left;
+        return dp[i][j] =  left + up;
     }
 };
