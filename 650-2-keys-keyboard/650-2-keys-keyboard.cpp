11e0b03bd0e21d3d187fb719a8ba7f4ecf3114f8
kunalpawa15-bit <83181573+kunalpawa15-bit@users.noreply.github.com>
2022-08-14 23:18:53 +0530

    Time: 178 ms (6.99%), Space: 155.7 MB (6.99%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/650-2-keys-keyboard.cpp
@@ -2,13 +2,15 @@ class Solution {
 public:
     int minSteps(int n) {
         if(n==1)return 0;
-        return 1 + f(1,1,n);
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        return 1 + f(1,1,n,dp);
     }
-    int f(int c,int v,int n)
+    int f(int c,int v,int n,vector<vector<int>>&dp)
     {
         if(c==n)return 0;
         if(c>n)return 1e9;
         
-        return min(1+f(c+v,v,n),2+f(c+c,c,n));
+        if(dp[c][v]!=-1)return dp[c][v];
+        return min(1+f(c+v,v,n,dp),2+f(c+c,c,n,dp));
     }
 };
