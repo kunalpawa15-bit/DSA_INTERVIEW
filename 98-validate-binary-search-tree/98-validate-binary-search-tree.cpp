f3fc396eaa4a3d489ac8f2da26393dcfd92ef40c
kunalpawa15-bit <83181573+kunalpawa15-bit@users.noreply.github.com>
2022-08-20 17:03:30 +0530

    Time: 15 ms (77.39%), Space: 21.6 MB (88.99%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -1,19 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
-        
-        if(isBST(root,NULL,NULL))return true;
-        
+       if(isbst(root,NULL,NULL)==true)return true;
         return false;
     }
     
-    bool isBST(TreeNode* root,TreeNode* min,TreeNode* max)
+    bool isbst(TreeNode* root,TreeNode* min,TreeNode* max)
     {
         if(root==NULL)return true;
+        if(min!=NULL && root->val<=min->val)return false;
+        if(max!=NULL && root->val>=max->val)return false;
         
-        if(min!= NULL && root->val <= min->val)return false;
-        if(max != NULL && root->val >= max->val)return false;
-        
-        return isBST(root->left,min,root) && isBST(root->right,root,max);
+        return (isbst(root->left,min,root) && isbst(root->right,root,max));
     }
 };
