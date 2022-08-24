63b97457e0e6b710858dadc0efa5b19c3826c245
kunalpawa15-bit <83181573+kunalpawa15-bit@users.noreply.github.com>
2022-08-24 22:58:48 +0530

    Time: 67 ms (82.25%), Space: 30.1 MB (88.40%) - LeetHub

--- 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.cpp
+++ 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.cpp
@@ -1,23 +1,38 @@
 class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
-        int sum=0;
-        vector<int>s(k);
-        s[sum]++;
+       int sum = 0;
         int count = 0;
-        
+        vector<int>v(k);
+        v[sum]++;
         for(int i=0;i<nums.size();i++)
         {
             sum = (sum + nums[i])%k;
+            if(sum<0)sum= sum + k;
             
-            if(sum<0)sum = sum + k;
-            
-            count = count + s[sum];
-            s[sum]++;
+            count = count + v[sum];
+            v[sum]++;
         }
-        
         return count;
-        
     }
 };
-
+// class Solution {
+// public:
+//     int subarraysDivByK(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         int sum = 0;
+//         int count = 0;
+//         for(int i=0;i<n;i++)
+//         {
+//             sum = nums[i];
+//             if(sum%k==0)count++;
+//             for(int j=i+1;j<n;j++)
+//             {
+//                 sum = sum + nums[j];
+//                 if(sum%k==0)
+//                     count++;
+//             }
+//         }
+//         return count;
+//     }
+// };
