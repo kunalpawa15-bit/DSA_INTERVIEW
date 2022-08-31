cf342b5f05d8d9ed06981fab9e2069360d29061c
kunalpawa15-bit <83181573+kunalpawa15-bit@users.noreply.github.com>
2022-08-31 16:38:39 +0530

    Time: 82 ms (6.06%), Space: 13.2 MB (23.81%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -5,15 +5,16 @@ public:
         int m = text2.length();
         vector<vector<int>>dp(n,vector<int>(m,-1));
         return f(n-1,m-1,text1,text2,dp);
-        
     }
-    
     int f(int i,int j,string &s1,string &s2,vector<vector<int>>&dp)
     {
         if(i<0 || j<0)return 0;
-        
         if(dp[i][j]!=-1)return dp[i][j];
-        if(s1[i]==s2[j])return dp[i][j] =  1+f(i-1,j-1,s1,s2,dp);
-        return dp[i][j] =  0 + max(f(i-1,j,s1,s2,dp),f(i,j-1,s1,s2,dp));
+        if(s1[i]==s2[j])
+        {
+            return dp[i][j]=1 + f(i-1,j-1,s1,s2,dp);
+        }
+        
+        return dp[i][j] = 0+ max(f(i-1,j,s1,s2,dp),f(i,j-1,s1,s2,dp));
     }
 };
