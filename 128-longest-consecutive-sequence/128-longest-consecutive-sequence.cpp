8916a00be83f5f1c6afb15a09273dc74aed4fd13
kunalpawa15-bit <83181573+kunalpawa15-bit@users.noreply.github.com>
2022-09-01 17:52:08 +0530

    Time: 2418 ms (5.00%), Space: 51.2 MB (26.33%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -2,28 +2,61 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         
-        int n  =nums.size();
-        sort(nums.begin(),nums.end());
-        int count=1;
-        int maxi = 1;
-        for(int i=1;i<n;i++)
+        set<int>s;
+        int curr=0;
+        int maxi= 0;
+        int sum;
+        int n = nums.size();
+        for(int i=0;i<n;i++)
         {
-            if(nums[i] == nums[i-1]+1)
-            {
-                count++;
-            }
-            else if(nums[i]==nums[i-1])
-                continue;
-            else
-            {
-                count=1;
-            }
-            
-            maxi = max(maxi,count);
+            s.insert(nums[i]);
         }
-        
-        if(n==0)return 0;
-        else
-            return maxi;
+        for(int i=0;i<n;i++)
+        {
+            if(!s.count(nums[i]-1))
+            {
+                curr = nums[i];
+                sum=1;
+                
+                while(s.count(curr+1))
+                {
+                    curr++;
+                    sum++;
+                }
+                
+                maxi = max(maxi,sum);
+                
+            }
+        }
+        return maxi;
     }
 };
+// class Solution {
+// public:
+//     int longestConsecutive(vector<int>& nums) {
+        
+//         int n  =nums.size();
+//         sort(nums.begin(),nums.end());
+//         int count=1;
+//         int maxi = 1;
+//         for(int i=1;i<n;i++)
+//         {
+//             if(nums[i] == nums[i-1]+1)
+//             {
+//                 count++;
+//             }
+//             else if(nums[i]==nums[i-1])
+//                 continue;
+//             else
+//             {
+//                 count=1;
+//             }
+            
+//             maxi = max(maxi,count);
+//         }
+        
+//         if(n==0)return 0;
+//         else
+//             return maxi;
+//     }
+// };
